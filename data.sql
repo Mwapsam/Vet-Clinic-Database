38148b58774c7d8f85a6b45929588f1d3fda466c
Mwapsam <mwapsam@gmail.com>
2022-03-23 14:41:29 +0200

    query and update animal database

--- data.sql
+++ data.sql
@@ -8,3 +8,15 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Pikachu', '01/07/2021', 15.04,false, 1 );
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Devimon', '05/12/2017', 11, true, 5);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Charmander', '08/02/2020', 11, false, 0);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Plantmon', '15/11/2021', 5.7, true, 2);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Squirtle', '04/02/1993', 12.13, false, 3);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Angemon', '06/12/2005', 45, true, 1);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Boarmon', '06/07/2005', 20.4, true, 3);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Blossom', '14/05/2022', 22, true, 4);
--- queries.sql
+++ queries.sql
@@ -8,3 +8,74 @@ SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = true;
 SELECT name from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+/* Start transaction query for species column */
+
+BEGIN;
+  UPDATE animals SET species = 'unspecified';
+
+  select * from animals;
+
+ROLLBACK TRANSACTION;
+
+/* end transaction query for species column */
+
+/* Start transaction query for updating species column for animals that have name ending in mon and those that do not have species already set */
+
+BEGIN;
+
+    UPDATE animals SET species = 'digimon' WHERE name LIKE 'mon%';
+
+    UPDATE animals SET species = 'pokemon' WHERE species = '';
+
+    /* Confirm updates */
+
+    SELECT * FROM animals;
+
+COMMIT WORK;
+
+/* END transaction */
+
+/* Start transaction to delete all records in the animals table */
+
+BEGIN;
+    DELETE FROM animals;
+ROLLBACK;
+
+/* End transaction with rollback */
+
+/* Start transaction for multiple operations */
+
+BEGIN;
+
+    DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+    SAVEPOINT deleteAnimalsDob;
+
+    UPDATE animals SET weight_kg = weight_kg * -1;
+
+    ROLLBACK TO deleteAnimalsDob;
+
+    UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+    RELEASE SAVEPOINT deleteAnimalsDob;
+
+COMMIT;
+/* End transaction */
+
+/* WRITE QUERIES TO ANSWER QUESTIONS */
+
+SELECT COUNT(id) as animals_count FROM animals;
+
+SELECT COUNT(id) as never_tried_escape FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) as average_weight FROM animals;
+
+SELECT SUM(escape_attempts) as escape_count, neutered FROM animals GROUP BY neutered ORDER BY escape_count DESC LIMIT 1;
+
+SELECT species, MAX(weight_kg) as maximum_weight, MIN(weight_kg) as minimum_weight FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+/* END */
